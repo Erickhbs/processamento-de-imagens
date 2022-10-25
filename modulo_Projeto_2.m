298bde252c7bf896fd4dba3d079b0be7b0d0a612
ehbs <ehbs.13579@gmail.com>
2022-10-25 05:14:40 -0300

    Codigo de cropagem das imagens feita

--- modulo_Projeto_2.m
+++ modulo_Projeto_2.m
@@ -1,39 +1,35 @@
-close all
 clear all
+close all
 pkg load image
 
-im = imread('C:\PDI\feijaoPreto.jpeg');
+imCinza = imread('C:\PDI\feijaoPreto.jpg');
 
-imCinza = zeros(size(im,1)/2,size(im,2)/2);
 
-y = 1;
-x = 1;
-for(i=1:2:size(im,1))
-    for(j=1:2:size(im,2))
-        imCinza(y,x) = im(i,j);
-        x++;
+% Pegando a distância entre cada linha
+distRows = floor((size(imCinza,1)/5));
+distRows = uint64(distRows)
+
+% Pegando a distância entre cada coluna
+distColumns = floor((size(imCinza,2)/5));
+distColumns = uint64(distColumns)
+
+% Andando de posição em posição para cropar imagem
+for(i=uint64(1):1:5)
+    for(j=uint64(1):1:5)
+        % Matriz para receber imagem cropada
+        imCropada = uint8(zeros(distRows,distColumns));
+        contY = 1;
+        contX = 1;
+        % Recebendo imagem cropada
+        for(ii=(distRows*i)-distRows+1:1:distRows*i)
+            for(jj=(distColumns*j)-distColumns+1:1:distColumns*j)
+                imCropada(contY,contX) = imCinza(ii,jj);
+                contX++;
+            endfor
+            contX=1;
+            contY++;
+        endfor
+        % Cropando imagem
+        imwrite(imCropada,strcat('C:\Users\ehbs1\OneDrive\Documentos\Crop\',mat2str(i),',',mat2str(j),'.jpg'))
     endfor
-    y++;
 endfor
-
-figure('Name','Imagem pequena')
-imshow(imCinza)
-
-%distColumns = floor(size(imCinza,2)/5);
-%distRows = floor(size(imCinza,1)/5);
-%C:\Users\ehbs1\OneDrive\Documentos\Crop
-%for(i=1:5)
-%    for(j=1:5)
-%        imCropada=zeros(distRows,distColumns);
-%        contY = 1;
-%        contX = 1;
-%        for(ii=(distRows*i)-distRows+1:distRows*i)
-%            for(jj=(distColumns*i)-distColumns+1:distColumns*j)
-%                imCropada(contY,contX) = imCinza(ii,jj);
-%                contX++;
-%            endfor
-%            contY++;
-%        endfor
-%        imwrite(imCropada,strcat('C:\Users\ehbs1\OneDrive\Documentos\Crop\',mat2str(i),',',mat2str(j),'.jpg'))
-%    endfor
-%endfor
